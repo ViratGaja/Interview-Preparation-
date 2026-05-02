31e0651c9c9c46a0fbed1397ef94c0df72af454b
Gajapathi K <113296010+ViratGaja@users.noreply.github.com>
2026-05-02 17:37:30 +0530

    changes

--- React/learn/src/App.jsx
+++ React/learn/src/App.jsx
@@ -146,32 +146,69 @@
   
 // })
 
-import React from "react";
-import { useContext } from "react";
-import { createContext } from "react";
+// import React from "react";
+// import { useContext } from "react";
+// import { createContext } from "react";
 
 
-const Data=createContext();
+// const Data=createContext();
 
 
-function App(){
+// function App(){
 
-  const value="king"
+//   const value="king"
 
 
-  return(
-    <Data.Provider value={value}>
-      <Child />
-    </Data.Provider>
+//   return(
+//     <Data.Provider value={value}>
+//       <Child />
+//     </Data.Provider>
+//   )
+// }
+
+
+// function Child(){
+//   const Use=useContext(Data)
+
+//   return <h1>{Use}</h1>
+// }
+
+// export default App
+
+
+
+function reducer(state,action){
+
+  switch(action.type){
+    case "increment":
+      return {count:state.count+1};
+    case "decrement":
+      return {count: state.count-1}
+    default:
+      state;
+  }
+
+
+}
+
+
+
+
+
+
+import React, { useReducer } from 'react'
+
+const App = () => {
+  const [state,dispatch]=useReducer(reducer,{count:0})
+  return (
+    <div>
+      <h1>{state.count}</h1>
+      <button onClick={()=>dispatch({type:"increment"})}>increment</button>
+
+      <button onClick={()=>dispatch({type:"decrement"})}>Decrement</button>
+
+    </div>
   )
 }
 
-
-function Child(){
-  const Use=useContext(Data)
-
-  return <h1>{Use}</h1>
-}
-
 export default App
-
