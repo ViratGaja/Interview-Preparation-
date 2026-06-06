e9977d2201f2d642647747a451d5b230097b42c6
Gajapathi K <113296010+ViratGaja@users.noreply.github.com>
2026-06-06 22:29:02 +0530

    consistency

--- L2-React/src/App.jsx
+++ L2-React/src/App.jsx
@@ -1,61 +1,67 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const[formData,setFormData]=useState({
-    name:"",
-    email:"",
-    password:""
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
   })
-  const [error,setError]=useState({})
+  const [error, setError] = useState({})
 
-  const handleForm=(e)=>{
+  const handleForm = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]:e.target.value
+      [e.target.name]: e.target.value
     })
   }
 
-  const submitForm=(e)=>{
+  const HandleSubmit = (e) => {
     e.preventDefault();
 
-    let newError={};
 
-    if(formData.name===""){
-      newError.name="Name field is required"
+    let newError = {};
+
+    if (formData.name === "") {
+      newError.name = "Please enter the name"
     }
-    if(formData.email===""){
-      newError.email="Email field is required"
+
+    if (formData.email === "") {
+      newError.email = "please enter the email"
     }
-    else if(!formData.email.includes("@")){
-      newError.email="enter the valid email"
+    else if (!formData.email.includes("@")) {
+      newError.email = "please enter the @"
     }
-     if (formData.password === "") {
-      newError.password = "Password is required"
-     }
-     else if(formData.password.length<6){
-      newError.password="password must be 6 character"
-     }
 
-     setError(newError)
 
-     if(Object.keys(newError).length===0){
-      alert("submit Success")
-     }
+    if (formData.password === "") {
+      newError.password = "Please enter the password"
+    }
+    else if (formData.password.length < 6) {
+      newError.password = "must be password above 6"
+    }
 
+    setError(newError);
+
+    if (Object.keys(newError).length === 0) {
+      alert("Form Submitted")
+    }
   }
+
   return (
     <div>
 
-      <form>
-        <input type="text" value={formData.name} onChange={handleForm} placeholder='name' name="name" id="" />
-        {error.name &&<p style={{color:"red"}}>{error.name}</p>}<br /> <br />
-        
-        <input type="email" value={formData.email} onChange={handleForm} placeholder='email' name="email" id="" />
-        {error.name &&<p style={{color:"red"}}>{error.email}</p>}<br /> <br />
-        <input type="password"value={formData.password} onChange={handleForm} placeholder='password' name="password" id="" />
-        {error.name &&<p style={{color:"red"}}>{error.password}</p>}<br /> <br />
-        <button onClick={submitForm}>Submit</button>
+
+      <form action="">
+
+        <input type="text" name='name' value={formData.name} onChange={handleForm} placeholder='Name' /> <br /> <br />
+        {error.name && <p style={{ color: "red" }}>{error.name}</p>}
+        <input type="email" name='email' value={formData.email} onChange={handleForm} placeholder='email' /><br /> <br />
+        {error.email && <p style={{ color: "red" }}>{error.email}</p>}
+        <input type="password" name='password' value={formData.password} onChange={handleForm} placeholder='password' /> <br /><br />
+        {error.password && <p style={{ color: "red" }}>{error.password}</p>}
+        <button type='submit' onClick={HandleSubmit} >Submit</button>
       </form>
+
 
 
     </div>
---
+++ throttle.js
@@ -0,0 +1,21 @@
+
+
+function throttle(another,delay){
+
+    let time=0;
+    return function(){
+        let current=Date.now();
+        if(current-time>=delay){
+            another()
+        }
+        time=current
+    }
+}
+function another(){
+    console.log("Hello everybody ");
+    
+}
+
+const final_result=throttle(another,2000);
+
+final_result()
