ab17011adda966ad4f0fb265fcd1a4a53e27b2c6
Gajapathi K <113296010+ViratGaja@users.noreply.github.com>
2026-06-13 18:10:52 +0530

    changes

--- L2-React/src/App.jsx
+++ L2-React/src/App.jsx
@@ -1,69 +1,11 @@
-import React, { useState } from 'react'
-
+import React from 'react'
+import Child from './Child'
 const App = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: ""
-  })
-  const [error, setError] = useState({})
-
-  const handleForm = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    })
-  }
-
-  const HandleSubmit = (e) => {
-    e.preventDefault();
-
-
-    let newError = {};
-
-    if (formData.name === "") {
-      newError.name = "Please enter the name"
-    }
-
-    if (formData.email === "") {
-      newError.email = "please enter the email"
-    }
-    else if (!formData.email.includes("@")) {
-      newError.email = "please enter the @"
-    }
-
-
-    if (formData.password === "") {
-      newError.password = "Please enter the password"
-    }
-    else if (formData.password.length < 6) {
-      newError.password = "must be password above 6"
-    }
-
-    setError(newError);
-
-    if (Object.keys(newError).length === 0) {
-      alert("Form Submitted")
-    }
-  }
-
+     var name="king"
   return (
+   
     <div>
-
-
-      <form action="">
-
-        <input type="text" name='name' value={formData.name} onChange={handleForm} placeholder='Name' /> <br /> <br />
-        {error.name && <p style={{ color: "red" }}>{error.name}</p>}
-        <input type="email" name='email' value={formData.email} onChange={handleForm} placeholder='email' /><br /> <br />
-        {error.email && <p style={{ color: "red" }}>{error.email}</p>}
-        <input type="password" name='password' value={formData.password} onChange={handleForm} placeholder='password' /> <br /><br />
-        {error.password && <p style={{ color: "red" }}>{error.password}</p>}
-        <button type='submit' onClick={HandleSubmit} >Submit</button>
-      </form>
-
-
-
+        <Child name={name}/>
     </div>
   )
 }
---
+++ form_validation.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const[formData,setFormData]=useState({
+    name:"",
+    email:"",
+    password:""
+  })
+  const [error,setError]=useState({})
+
+  const handleForm=(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.name]:e.target.value
+    })
+  }
+
+  const submitForm=(e)=>{
+    e.preventDefault();
+
+    let newError={};
+    
+    if(formData.name===""){
+      newError.name="Name field is required"
+    }
+    if(formData.email===""){
+      newError.email="Email field is required"
+    }
+    else if(!formData.email.includes("@")){
+      newError.email="enter the valid email"
+    }
+     if (formData.password === "") {
+      newError.password = "Password is required"
+     }
+     else if(formData.password.length<6){
+      newError.password="password must be 6 character"
+     }
+
+     setError(newError)
+
+     if(Object.keys(newError).length===0){
+      alert("submit Success")
+     }
+
+  }
+  return (
+    <div>
+
+      <form>
+        <input type="text" value={formData.name} onChange={handleForm} placeholder='name' name="name" id="" />
+        {error.name &&<p style={{color:"red"}}>{error.name}</p>}<br /> <br />
+        
+        <input type="email" value={formData.email} onChange={handleForm} placeholder='email' name="email" id="" />
+        {error.name &&<p style={{color:"red"}}>{error.email}</p>}<br /> <br />
+        <input type="password"value={formData.password} onChange={handleForm} placeholder='password' name="password" id="" />
+        {error.name &&<p style={{color:"red"}}>{error.password}</p>}<br /> <br />
+        <button onClick={submitForm}>Submit</button>
+      </form>
+
+
+    </div>
+  )
+}
+
+export default App
